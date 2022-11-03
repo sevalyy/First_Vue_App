fd4d6799cf3ef41275408b52bbb899d5d444f1e5
sevalyy <seval@yamanyar.com>
2022-11-03 16:57:57 +0100

    using method inside app.js

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const app = Vue.createApp({
   template: `
         <h1>{{title}}</h1>
         <p>{{content}}</p>
+        <p>total number: {{number}}</p>
+        <button v-on:click="addApp()">Click here </button>
 
     `,
   data() {
     return {
       title: "Web page with Vue",
       content: "We have just started learning Vue js ",
+      number: 5,
     };
+  },
+  methods: {
+    addApp() {
+      console.log("addApp function working! ");
+      this.number++;
+    },
   },
 });
 app.mount("#app");
