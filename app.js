6e97023c25bccc16c183d99bf5768a13ac319f5f
sevalyy <seval@yamanyar.com>
2022-11-03 17:08:48 +0100

    more about methode

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const app = Vue.createApp({
   template: `
         <h1>{{title}}</h1>
         <p>{{content}}</p>
-        <p>total number: {{number}}</p>
+        <p>Total number: {{number}}</p>
         <button v-on:click="addApp()">Click here </button>
+        <p @click="addApp()">{{article}}</p>
+
 
     `,
   data() {
@@ -11,12 +13,14 @@ const app = Vue.createApp({
       title: "Web page with Vue",
       content: "We have just started learning Vue js ",
       number: 5,
+      article: "Click this article for increasing number ",
     };
   },
   methods: {
     addApp() {
       console.log("addApp function working! ");
       this.number++;
+      this.article = `Now ${this.number} apss working!`;
     },
   },
 });
